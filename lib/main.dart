d69b7ba3aaf4bc0bf2bd7fe8abfd1b879e682b00
henrique-vedois <75740946+henrique-vedois@users.noreply.github.com>
2021-04-24 08:45:34 -0300

    exception handled at the end of the questions

--- lib/main.dart
+++ lib/main.dart
@@ -9,47 +9,56 @@ main() {
 class _PerguntaAppState extends State<PerguntaApp> {
   var _perguntaSelecionada = 0;
 
+  final _perguntas = [
+    {
+      'texto': 'Qual é a sua cor favorita?',
+      'respostas': ['Preto', 'Branco', 'Verde', 'Amarelo'],
+    },
+    {
+      'texto': 'Qual é o seu animal favorito?',
+      'respostas': ['Humano', 'Macaco', 'Cobra', 'Tigre'],
+    },
+    {
+      'texto': 'Qual é o seu nome favorito?',
+      'respostas': ['Henrique', 'Dico', 'Rodrigo', 'Tico'],
+    }
+  ];
+
   void _responder() {
-    setState(() {
-      _perguntaSelecionada++;
-      print(_perguntaSelecionada);
-    });
+    if (temPerguntaSelecionada) {
+      setState(() {
+        _perguntaSelecionada++;
+      });
+    }
+  }
+
+  bool get temPerguntaSelecionada {
+    return _perguntaSelecionada < _perguntas.length;
   }
 
   @override
   Widget build(BuildContext context) {
-    final perguntas = [
-      {
-        'texto': 'Qual é a sua cor favorita?',
-        'respostas': ['Preto', 'Branco', 'Verde', 'Amarelo'],
-      },
-      {
-        'texto': 'Qual é o seu animal favorito?',
-        'respostas': ['Humano', 'Macaco', 'Cobra', 'Tigre'],
-      },
-      {
-        'texto': 'Qual é o seu nome favorito?',
-        'respostas': ['Henrique', 'Dico', 'Rodrigo', 'Tico'],
-      }
-    ];
+    List<String> respostas = temPerguntaSelecionada
+        ? _perguntas[_perguntaSelecionada]['respostas']
+        : null;
 
-    List<Widget> respostas = [];
-
-    for (String textoResp in perguntas[_perguntaSelecionada]['respostas']) {
-      respostas.add(Resposta(textoResp, _responder));
-    }
+    //for (String textoResp in respostas) {
+    //  widgets.add(Resposta(textoResp, _responder));
+    //}
 
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('Perguntas'),
         ),
-        body: Column(
-          children: [
-            Questao(perguntas[_perguntaSelecionada]['texto']),
-            ...respostas
-          ],
-        ),
+        body: temPerguntaSelecionada
+            ? Column(
+                children: [
+                  Questao(_perguntas[_perguntaSelecionada]['texto']),
+                  ...respostas.map((t) => Resposta(t, _responder)).toList(),
+                ],
+              )
+            : null,
       ),
     );
   }
